c8e09fb24d0829deb041a67d1d85506bb6a37070
Himanshu <129241828+himanshu140505@users.noreply.github.com>
2023-10-11 10:49:19 +0530

    Create area-perimeter.c

---
+++ area-perimeter.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(void){
+	float r;
+	float pi = 3.14;
+	printf("ENTER THE VALUE OF R -> RADIUS : ");
+	scanf("%f",&r);
+	float area = pi * r * r;
+	float circumference = 2*pi*r;
+	printf("THE AREA OF THE CIRCLE OF RADIUS %f is %f \n", r, area);
+	printf("THE CIRCUMFERENCE OF THE CIRCLE OF RADIUS %f is %f ", r, circumference);
+	
+}
